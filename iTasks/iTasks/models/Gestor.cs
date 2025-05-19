6d2ab3a6fedf96d75b9a2c67219b83cdeda0ceb4
[2231437@my.ipleiria.pt] <2231437@my.ipleiria.pt>
2025-05-19 18:07:54 +0100

    Adicionar Projeto

---
+++ iTasks/iTasks/models/Gestor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iTasks.models
+{
+    internal class Gestor
+    {
+        public string Departamento { get; set; }
+        public string GereUtilizador { get; set; }
+    }
+}
