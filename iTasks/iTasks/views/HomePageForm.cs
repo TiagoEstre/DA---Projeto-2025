e3dfff191a3b8f30c16ce52122da2abbf5bf5a75
[2231437@my.ipleiria.pt] <2231437@my.ipleiria.pt>
2025-05-31 16:52:24 +0100

    Desenho do v1 do TaskDetailForm

--- iTasks/iTasks/views/HomePageForm.cs
+++ iTasks/iTasks/views/HomePageForm.cs
@@ -73,6 +73,7 @@ namespace iTasks.views
         {
             l_NameForm.Text = "Detalhes Tarefas";
             pb_CurrentChildForm.Image = iTasks.Properties.Resources.icons8_tasks_96;
+            trocarForm(new TaskDetailForm());
         }
 
         private void b_OngoingTasks_Click(object sender, EventArgs e)
